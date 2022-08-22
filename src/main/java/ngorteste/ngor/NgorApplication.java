96b96b1ea2e68b86c38071192b9966314ba89982
alieu samb <alieusamb@air-de-alieu.lan>
2022-08-22 12:46:47 +0000

    Initial commit

---
+++ src/main/java/ngorteste/ngor/NgorApplication.java
@@ -0,0 +1,13 @@
+package ngorteste.ngor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ngorteste/ngor/NgorApplicationTests.java
@@ -0,0 +1,13 @@
+package ngorteste.ngor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
